d582f9f12b49c957ceecb5244ad01b28c22913a8
Edaliwen <amandine.valtat@gmail.com>
2022-10-26 12:02:50 +0200

    Connexion API
    Création d'un service de connexion à l'API
    Installation des packages asset et http-client
    Suppression du controller test 'SayHelloController'

---
+++ src/Controller/DisplayListController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CallApiService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class DisplayListController extends AbstractController
+{
+   
+    #[Route('/display/list', name: 'app_display_list')]
+    public function index(CallApiService $callApiService): Response
+    {    
+        dd($callApiService->getVersaillesData());
+        return $this->render('display_list/index.html.twig', [
+            'controller_name' => 'DisplayListController',
+        ]);
+    }
+}
