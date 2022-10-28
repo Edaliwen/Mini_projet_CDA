6a81269f7643d3051b370d280d2de4201c8979ef
Edaliwen <amandine.valtat@gmail.com>
2022-10-28 12:09:49 +0200

    ajout use

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,8 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Bundle\DebugBundle;
+use App\Repository\UserRepository;
+use App\Entity\User;
 
 class Kernel extends BaseKernel
 {
